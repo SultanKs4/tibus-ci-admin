18da42b0d51c7f8f59ec30e7df2c96b821be791f
rizalanhari <rizalanhari6@gmail.com>
2020-05-16 19:27:39 +0700

    fix(controller&model): fix API 2

--- application/models/Payment_model.php
+++ application/models/Payment_model.php
@@ -7,7 +7,7 @@ class payment_model extends CI_Model
 
     public function getPayment($id = null)
     {
-        $this->db->select('payment.id, payment.id_tiket, akun.email as akun, payment.total, payment_method.name as method, payment.bukti_bayar, payment_status.name as status');
+        $this->db->select('payment.id, akun.email as akun, payment.total, payment_method.name as method, payment.bukti_bayar, payment_status.name as status');
         $this->db->from('payment');
         $this->db->join('akun', 'payment.id_akun = akun.id');
         $this->db->join('payment_method', 'payment.metode_bayar = payment_method.id');
